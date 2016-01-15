97ff50dba88621834d1610046b4cce8cc6fc8b2a
juaniiie <becjuana@gmail.com>
2016-01-14 23:04:31 -0800

    Feature: All controller and services are functional, with mock logic

--- client/app.js
+++ client/app.js
@@ -3,7 +3,9 @@ import uirouter from 'angular-ui-router';
 
 import routing from './app.config';
 import HomeController from './controllers/HomeController';
+import HomeService from './services/services.js';
 
 angular.module('app', [uirouter])
     .config(routing)
-    .controller('HomeController', HomeController);
+    .controller('HomeController', HomeController)
+    .service('HomeService', HomeService);
--- client/controllers/homeController.js
+++ client/controllers/homeController.js
@@ -1,10 +1,17 @@
 export default class HomeController {
     
-    constructor(){
+    constructor(HomeService){
         this.name = 'World';
+        this.random = HomeService;
     }
 
     changeName() {
         this.name = 'angular-tips';
     }
+
+    randomName() {
+        this.name = this.random.getName();
+    }
 }
+
+HomeController.$inject = ['HomeService'];

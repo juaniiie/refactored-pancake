01d2a38011495aeff0dd0bd8068df9a7ca7bd7fc
juaniiie <becjuana@gmail.com>
2016-01-14 19:40:22 -0800

    Feature: Adds module state configuration, injects ng-app in index.html

--- client/app.config.js
+++ client/app.config.js
@@ -1,15 +1,14 @@
 routing.$inject = ['$locationProvider', '$urlRouterProvider', '$stateProvider'];
 
 export default function routing($locationProvider, $urlRouterProvider, $stateProvider) {
-    
     $locationProvider.html5Mode(true);
     
-    $urlRouterProvider('/home');
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider
         .state('home', {
-            url: '/home',
-            templateUrl: 'client/templates/home.html',
+            url: '/',
+            template: require('./templates/home.html'),
             controller: 'HomeController',
             controllerAs: 'home'
         });
--- client/app.js
+++ client/app.js
@@ -1,6 +1,9 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
-import routing from './app.config';
 
-angular.module('app', [])
-    .config(routing);
+import routing from './app.config';
+import HomeController from './controllers/HomeController';
+
+angular.module('app', [uirouter])
+    .config(routing)
+    .controller('HomeController', HomeController);
